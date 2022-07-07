420231d446699939587ede43215392cc63e14362
JonOfScience <jdcrofts31@gmail.com>
2022-07-07 10:13:13 +0100

    Modifications

--- Week 2/Labs/DataTypes_Lab_Starter/DataTypes_Lib/Methods.cs
+++ Week 2/Labs/DataTypes_Lab_Starter/DataTypes_Lib/Methods.cs
@@ -7,6 +7,9 @@ namespace DataTypes_Lib
         // write a method to return the product of all numbers from 1 to n inclusive
         public static long Factorial(int n)
         {
+            if (n < 1)
+                throw new ArgumentOutOfRangeException();
+            
             long result = 1;
             for (int i = 1; i < n+1; i++)
                 result *= (long)i;
