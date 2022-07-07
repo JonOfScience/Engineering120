ea41fcad46bc19249e6b23bdea404815d5601e63
JonOfScience <jdcrofts31@gmail.com>
2022-07-07 14:06:42 +0100

    Day 9 session 1

--- Week 2/MoreDataTypes/MoreDataTypesApp/Program.cs
+++ Week 2/MoreDataTypes/MoreDataTypesApp/Program.cs
@@ -6,6 +6,7 @@ namespace MoreDataTypesApp
     {
         static void Main(string[] args)
         {
+            #region Strings and String Methods
             //Console.WriteLine("Hello, World!");
             //String message = "I like donuts";
             //Console.WriteLine(message);
@@ -16,8 +17,49 @@ namespace MoreDataTypesApp
             //Console.WriteLine(newnew);
 
             Console.WriteLine(StringExercise(" C# list fundamentals "));
+            #endregion
+
+            #region StringBuilder
             Console.WriteLine(StringBuilderExercise(" C# list fundamentals "));
+            #endregion
+
+            #region String Interpolation
+            string a = "A";
+            string b = "B";
+            string aAndB = a + b;
+            Console.WriteLine(aAndB);
+            aAndB = String.Concat(a, b);
+            Console.WriteLine(aAndB);
+            string[] arrayOfStrings = { "hello", "world" };
+            Console.WriteLine(String.Concat(arrayOfStrings));
+            char[] arrayOfChars = { 'a', 'b', 'c' };
+            Console.WriteLine(String.Concat(arrayOfChars));
+            Console.WriteLine("Your blood type is " + a + b + ".");
+            Console.WriteLine($"Your blood type is {a}{b.ToLower()}.");
+
+            int num1 = 2;
+            int num2 = 7;
+            string logOfNum1Num2 = $"The log to base {num1} of {num2} is {Math.Log(num2, num1):N3}";
+            Console.WriteLine(logOfNum1Num2);
+            string money = $"The change is {(num2 - num1):C}";
+            Console.WriteLine(money);
+            #endregion
+
+            #region String parsing
+            ParsingStrings();
+            #endregion
         }
+
+        public static void ParsingStrings()
+        {
+            Console.WriteLine("How many cars do you own?");
+            string input = Console.ReadLine();
+            //int numOfCars = Int32.Parse(input);
+            var success = Int32.TryParse(input, out int numOfCars);
+            Console.WriteLine(numOfCars);
+            Console.WriteLine(success);
+        }
+
 
         public static string StringExercise(string myString)
         {
