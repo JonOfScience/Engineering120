66b719a7d551d2ee52760964cd64a18133a4cef8
JonOfScience <jdcrofts31@gmail.com>
2022-07-18 15:28:41 +0100

    FizzBuzz n=30

--- Week 3/FizzBuzz/FizzBuzzApp/FizzBuzzTests.cs
+++ Week 3/FizzBuzz/FizzBuzzApp/FizzBuzzTests.cs
@@ -39,6 +39,7 @@ namespace FizzBuzzApp
         }
 
         [TestCase(15, "1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz")]
+        [TestCase(30, "1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz 16 17 Fizz 19 Buzz Fizz 22 23 Fizz Buzz 26 Fizz 28 29 FizzBuzz")]
         public void GivenNDivisibleBy15_Returns_TheExpectedString(int n, string expectedResult)
         {
             Assert.That(Program.FizzBuzz(n), Is.EqualTo(expectedResult));
