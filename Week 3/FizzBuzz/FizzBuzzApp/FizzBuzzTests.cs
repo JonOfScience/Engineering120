5d649f378946dfb0493d99368f61a9a6b0bd3cf0
JonOfScience <jdcrofts31@gmail.com>
2022-07-18 15:20:29 +0100

    Refactored tests

--- Week 3/FizzBuzz/FizzBuzzApp/FizzBuzzTests.cs
+++ Week 3/FizzBuzz/FizzBuzzApp/FizzBuzzTests.cs
@@ -13,17 +13,19 @@ namespace FizzBuzzApp
         // 1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz 16 17 Fizz 19 Buzz Fizz 22 23 Fizz Buzz 26 Fizz 28 29 FizzBuzz
         [TestCase(1, "1")]
         [TestCase(2, "1 2")]
-        [TestCase(3, "1 2 Fizz")]
-        [TestCase(4, "1 2 Fizz 4")]
         [TestCase(5, "1 2 Fizz 4 Buzz")]
-        [TestCase(6, "1 2 Fizz 4 Buzz Fizz")]
         [TestCase(7, "1 2 Fizz 4 Buzz Fizz 7")]
-        [TestCase(8, "1 2 Fizz 4 Buzz Fizz 7 8")]
-        [TestCase(9, "1 2 Fizz 4 Buzz Fizz 7 8 Fizz")]
         [TestCase(10, "1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz")]
-        [TestCase(11, "1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11")]
-        [TestCase(12, "1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz")]
         public void GivenN_Return_TheExpectedString(int n, string expectedResult)
+        {
+            Assert.That(Program.FizzBuzz(n), Is.EqualTo(expectedResult));
+        }
+
+        [TestCase(3, "1 2 Fizz")]
+        [TestCase(6, "1 2 Fizz 4 Buzz Fizz")]
+        [TestCase(9, "1 2 Fizz 4 Buzz Fizz 7 8 Fizz")]
+        [TestCase(12, "1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz")]
+        public void GivenNDivisibleBy3_Returns_TheExpectedString(int n, string expectedResult)
         {
             Assert.That(Program.FizzBuzz(n), Is.EqualTo(expectedResult));
         }
